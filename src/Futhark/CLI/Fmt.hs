7e4cf4c16edc43e236c8d166e2f958dc40e8bfcb
Sunese <suneengtorp@live.dk>
2022-05-03 15:45:57 +0200

    working on flush pt. 2

--- src/Futhark/CLI/Fmt.hs
+++ src/Futhark/CLI/Fmt.hs
@@ -8,8 +8,13 @@ import Data.Function (fix)
 import qualified Data.Text.IO as T
 import Futhark.Util.Loc
 import Futhark.Util.Options
+import Futhark.Util.Pretty (prettyText)
 import Language.Futhark
 import Language.Futhark.Parser.Lexer.Tokens
+import System.Exit
+import System.IO
+import Data.Text
+import Data.List
 
 unpackCommentString :: L Token -> String
 unpackCommentString (L _ (COMMENT s)) = s
@@ -23,17 +28,20 @@ hasCommentBefore :: UncheckedDec -> L Token -> Bool
 hasCommentBefore dec comment =
   locOf dec > unpackTokenLoc comment
 
-flush decs comments = do
-  fix $ \loop -> do
-    let dec = head decs
-    let comment = head comments
-    if hasCommentBefore dec comment then do 
-      -- insert comment into IO or something
-      (decs', comments') <- loop
-      (tail decs', tail comments')
-    else do 
-      (decs', comments') <- loop
-      (tail decs', comments')
+flush :: Text -> [UncheckedDec] -> [UncheckedDec] -> [L Token] -> IO ()
+flush s decs decsQueue comments = do
+  -- check all decs
+  -- if one of them hasCommentBefore, then 'take' until that comment and
+  -- concatenate comment literal/string onto taken stuff and
+  -- proceed onto checking next dec
+
+  -- calculate how many chars to take by checking end position of each declaration (col) + 1 for linebreak
+  if hasCommentBefore (Data.List.head decs) (Data.List.head comments) 
+    then do
+      T.putStrLn s
+  else do 
+    flush s (Data.List.tail decs) (Data.List.head decs : decsQueue) comments
+
 
 -- | Run @futhark fmt@.
 main :: String -> [String] -> IO ()
@@ -42,11 +50,9 @@ main = mainWithOptions () [] "program" $ \args () ->
     [file] -> Just $ do
       s <- T.readFile file
       case parseWithComments file s of
-          (Left _, _) -> do
-            T.putStrLn "error"
-          (Right (Prog _ decs), comments) -> do
-            T.putStrLn "error"
-
-
-          
+          (Left e, _) -> do
+            hPutStrLn stderr "Parse error: "
+            exitWith $ ExitFailure 2
+          (Right Prog { progDoc = doc, progDecs = decs }, comments) -> do
+            flush s decs [] comments
     _ -> Nothing
