14d78d96da7b8b902e0da5b0c01f59674c73860d
Sunese <suneengtorp@live.dk>
2022-05-09 10:31:49 +0200

    working solution wih lines

--- src/Futhark/CLI/Fmt.hs
+++ src/Futhark/CLI/Fmt.hs
@@ -21,11 +21,7 @@ import Data.Text.IO (writeFile)
 import Data.List ( map, length, head, tail )
 import Data.Aeson.Key (toString)
 import Control.Monad.Cont (MonadIO(liftIO))
-import qualified System.Posix.Internals as T
-
-unpackCommentString :: L Token -> String
-unpackCommentString (L _ (COMMENT s)) = s
-unpackCommentString _ = error "unpackCommentString: not a comment"
+--import qualified System.Posix.Internals as T
 
 unpackTokenLoc :: L Token -> Loc
 unpackTokenLoc (L loc _) = loc
@@ -40,34 +36,8 @@ unpackTokLine tok = do
           case end of
             Pos _ line _ _ -> line
 
-unpackColTok :: L Token -> Int
-unpackColTok tok = do
-  case tok of 
-    L loc _ -> 
-      case loc of
-        NoLoc -> -1
-        Loc _ end -> 
-          case end of
-            Pos _ _ col _ -> col
-
-unpackColDec :: UncheckedDec -> Int
-unpackColDec dec = do
-  case dec of
-    ValDec vbb -> case vbb of
-      ValBind _ _ _ _ _ _ _ _ _ sl -> 
-        case sl of
-          sl' -> 
-            case sl' of
-              SrcLoc loc -> 
-                case loc of
-                  NoLoc -> -1
-                  Loc _ end -> 
-                    case end of
-                      Pos _ _ col _ -> col
-    _ -> -1
-
-unpackDecLine :: UncheckedDec -> Int
-unpackDecLine dec = do
+unpackDecEndLine :: UncheckedDec -> Int
+unpackDecEndLine dec = do
   case dec of
     ValDec vbb -> case vbb of
       ValBind _ _ _ _ _ _ _ _ _ sl -> 
@@ -82,76 +52,29 @@ unpackDecLine dec = do
                       Pos _ line _ _ -> line
     _ -> -1
 
--- check if there has been a comment before the given token
-hasCommentBefore :: UncheckedDec -> L Token -> Bool
-hasCommentBefore dec comment =
-  locOf dec > unpackTokenLoc comment
-
-noOfCharacters :: [UncheckedDec] -> Int
-noOfCharacters queue = do
-  sum (map unpackColDec queue) + length queue -- including newline chars
-
-format ::
-  Text ->
-  [UncheckedDec] ->
-  [UncheckedDec] ->
-  [UncheckedDec] ->
-  Text ->
-  Text ->
-  [L Token] ->
-  Text
-format s decs decs' decsQueue programText rest comments = do
-  case decs' of
-      [] -> append programText rest
-      _ -> do
-        case comments of
-          [] -> append programText rest
-          _ -> do
-            if hasCommentBefore (head decs') (head comments)
-              then do
-                format
-                  s
-                  decs
-                  (tail decs')
-                  (head decs' : decsQueue)
-                  (append
-                    (append programText (Data.Text.take (noOfCharacters decsQueue) s))
-                    (pack $ unpackCommentString (head comments)))
-                  (takeEnd (noOfCharacters decs - noOfCharacters decsQueue) s)
-                  (tail comments)
-            else do
-              format 
-                s
-                decs
-                (tail decs')
-                (head decs' : decsQueue)
-                programText 
-                rest
-                comments
-
-format_ :: [Text] -> [Text] -> [UncheckedDec] -> [L Token] -> Text
-format_ lines' linesRest decs comments = do
+format :: [Text] -> [Text] -> [UncheckedDec] -> [L Token] -> Text
+format lines' linesRest decs comments = do
   case decs of
     [] -> do
       case comments of
-        [] -> unlines lines'
+        [] -> unlines $ lines' ++ linesRest
         _ -> do -- no more decs, but still some comments, just eat the rest
           unlines $ lines' ++ linesRest
     _ -> do
       case comments of
         [] -> unlines $ lines' ++ linesRest -- no more comments, but still some decs, just eat the rest
         _ -> do
-          if hasCommentBefore (head decs) (head comments) then do
-            -- figure out if comment is on its own line or not
-            let commentLine = unpackTokLine (head comments) -- 2
-            let (a, linesRest') = Prelude.splitAt (commentLine - length lines') linesRest -- [com,main]
+          if locOf (head decs) > unpackTokenLoc (head comments) then do
+            let commentLine = unpackTokLine (head comments)
+            let (a, linesRest') = Prelude.splitAt (commentLine - length lines') linesRest
             let lines'' = lines' ++ a
-            format_ lines'' linesRest' (tail decs) (tail comments)
+            format lines'' linesRest' (tail decs) (tail comments)
           else do
             -- get end line pos of dec
-            let pos = unpackDecLine (head decs)
-            let (lines'', linesRest') = Prelude.splitAt (pos - length lines') linesRest
-            format_ lines'' linesRest' (tail decs) comments
+            let decLineNo = unpackDecEndLine (head decs)
+            let (lines'', linesRest') = Prelude.splitAt (decLineNo - length lines') linesRest
+            let lines''' = lines' ++ lines''
+            format lines''' linesRest' (tail decs) comments
 
 -- | Run @futhark fmt@.
 main :: String -> [String] -> IO ()
@@ -165,10 +88,6 @@ main = mainWithOptions () [] "program" $ \args () ->
             putStrLn "File has not been changed."
             exitWith $ ExitFailure 2
           (Right Prog { progDoc = doc, progDecs = decs }, comments) -> do
-            let s' = stripEnd s
-            --Data.Text.IO.writeFile file $ format s' decs decs [] "" s' comments
-            
-            --T.putStrLn $ format s' decs decs [] "" s' comments
-            T.putStrLn $ format_ [] (lines s') decs comments
-            --print $ lines s'
+            --Data.Text.IO.writeFile file $ format [] (lines s) decs comments
+            T.putStrLn $ format [] (lines s) decs comments
     _ -> Nothing
