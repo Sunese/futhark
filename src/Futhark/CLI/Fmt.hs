c4ae8c760f4f7fae4063d106af00d2b76b42f8ff
Sunese <suneengtorp@live.dk>
2022-05-22 20:44:01 +0200

    proper fmt error handling

--- src/Futhark/CLI/Fmt.hs
+++ src/Futhark/CLI/Fmt.hs
@@ -18,14 +18,8 @@ import Language.Futhark hiding (pretty)
 import Language.Futhark.Parser (parseWithComments, SyntaxError (syntaxErrorMsg, syntaxErrorLoc))
 import Language.Futhark.Parser.Lexer.Tokens
 import Prelude hiding (writeFile, unlines, lines)
-import Control.Monad.IO.Class (MonadIO, liftIO)
 import System.Exit
-import qualified Data.Set as Set
-import qualified Data.Map as Map
-import Data.Complex (Complex, realPart, imagPart)
-import Data.Ratio (Ratio(..), denominator, numerator)
 import Futhark.Util.Pretty
-import Futhark.CodeGen.ImpGen (comment)
 
 unpackTokLoc :: L Token -> Loc
 unpackTokLoc (L loc _) = loc
@@ -42,38 +36,6 @@ unpackTokEndLine tok = do
 
 unpackCommentString (L _ (COMMENT s)) = s
 unpackCommentString _ = error "unpackCommentString: not a comment"
-
-getDecEndLine :: UncheckedDec -> Int
-getDecEndLine dec = do
-  case locOf dec of
-    NoLoc -> error "NoLoc"
-    Loc _ end -> 
-      case end of
-        Pos _ line _ _ -> line
-
-format :: [T.Text] -> [T.Text] -> [UncheckedDec] -> [L Token] -> T.Text
-format srcLines srcLinesRest decs comments = do
-  case decs of
-    [] -> do
-      case comments of
-        [] -> T.unlines $ srcLines ++ srcLinesRest
-        _ -> do -- no more decs, but still some comments, just consume the rest
-          let srcLinesRest' = srcLines ++ [T.pack $ unpackCommentString $ head comments]
-          T.unlines $ srcLines ++ srcLinesRest
-    _ -> do
-      case comments of
-        [] -> T.unlines $ srcLines ++ srcLinesRest -- no more comments, but still some decs, just consume the rest
-        _ -> do
-          if locOf (head decs) > unpackTokLoc (head comments) then do
-            let commentEndLine = unpackTokEndLine (head comments)
-            let (srcLines', srcLinesRest') = Prelude.splitAt (commentEndLine - length srcLines) srcLinesRest
-            let srcLines'' = srcLines ++ srcLines'
-            format srcLines'' srcLinesRest' (tail decs) (tail comments)
-          else do
-            let decLineNo = getDecEndLine (head decs)
-            let (srcLines', srcLinesRest') = Prelude.splitAt (decLineNo - length srcLines) srcLinesRest
-            let srcLines'' = srcLines ++ srcLines'
-            format srcLines'' srcLinesRest' (tail decs) comments
 
 isLocatedBefore :: L Token -> UncheckedDec -> Bool
 isLocatedBefore comment dec = locOf dec > unpackTokLoc comment
@@ -114,7 +76,6 @@ flush decs comments doc = do
 
 ppr' :: [UncheckedDec] -> [L Token] -> Doc
 ppr' decs comments = stack $ punctuate line $ flush decs comments []
-   
 
 -- | Run @futhark fmt@.
 main :: String -> [String] -> IO ()
@@ -124,7 +85,8 @@ main = mainWithOptions () [] "program" $ \args () ->
       s <- TIO.readFile file
       case parseWithComments file s of
         (Left e, _) -> do
-          exitWith $ ExitFailure 2
+          putStr $ "Syntax error: " ++ locStr (syntaxErrorLoc e) ++ "\n" ++ syntaxErrorMsg e 
+          putStrLn "File has not been changed."
         (Right prog, comments) -> do
         --(Right prog, comments) -> do
           --TIO.putStrLn $ prettyText prog
