7bc4283fee76a2c1d99ce8958649ae42e2e3f11c
Sunese <suneengtorp@live.dk>
2022-05-26 22:40:09 +0200

    TODO: handle comments across decs

--- src/Futhark/CLI/Fmt.hs
+++ src/Futhark/CLI/Fmt.hs
@@ -43,92 +43,6 @@ checkComment coms' sloc' =
     [] -> False
     _ -> head coms' `isLocatedBefore` sloc'
 
-formatSource :: [UncheckedDec] -> [L Token] -> [Doc] -> Doc
-formatSource decs coms doc =
-  case decs of 
-    [] -> 
-      case coms of
-        [] -> stack doc
-        _ -> formatSource decs (tail coms) (doc ++ [text . unpackCommentString $ head coms])
-    _ ->
-      case coms of 
-        [] -> formatSource (tail decs) coms (doc ++ [ppr $ head decs])
-        _ -> do
-          let numberedComs = zip [0..] coms
-          let (decDoc, consumed) = formatDec (head decs) doc numberedComs
-          formatSource (tail decs) (drop consumed coms) (doc ++ decDoc)
-
-formatDec :: UncheckedDec -> [Doc] -> [(Int, L Token)] -> ([Doc], Int)
-formatDec dec doc numberedComs  = case dec of
-  ValDec vbb -> formatValBind vbb doc numberedComs 0
-  _ -> ([text "THIS SHOULD NOT APPEAR"], 0)
-
-formatValBind :: UncheckedValBind -> [Doc] -> [(Int, L Token)] -> Int -> ([Doc], Int)
-formatValBind (ValBind entry name retdecl rettype tparams args body docComment attrs sloc) doc numberedComs consumed =
-  case numberedComs of 
-    [] -> (doc ++ [constructDocWithoutBody </> indent 2 (ppr body)], consumed)
-    _ -> do
-      if checkComment (map snd numberedComs) sloc then do
-        let tmpDoc = doc ++ [align . text . unpackCommentString $ snd (head numberedComs)]
-        formatValBind 
-          (ValBind entry name retdecl rettype tparams args body docComment attrs sloc)
-          tmpDoc 
-          (tail numberedComs)
-          (fst $ head numberedComs)
-      else do
-        let (expDoc, consumed') = formatExp body numberedComs []
-        (doc ++ [constructDocWithoutBody </> indent 2 expDoc], consumed')
-  where
-    fun
-      | isJust entry = "entry"
-      | otherwise = "def"
-    retdecl' = case (ppr <$> unAnnot rettype) `mplus` (ppr <$> retdecl) of
-      Just rettype' -> colon <+> align rettype'
-      Nothing -> mempty
-    constructDocWithoutBody = 
-      mconcat (map ((<> line) . ppr) attrs)
-            <> prettyDocComment docComment
-            <> text fun
-            <+> pprName name
-            <+> align (sep (map ppr tparams ++ map ppr args))
-            <> retdecl'
-            <> text " ="
-
-formatExp :: UncheckedExp -> [(Int, L Token)] -> [Doc] -> (Doc, Int)
-formatExp exp numberedComs expDoc = do 
-  case exp of
-    Literal v sl -> formatLit v sl 
-    IntLit v _ sl -> formatLit v sl 
-    _ -> (text "THIS SHOULD NOT APPEAR", 0)
-  where 
-    formatLit v sl = 
-      case numberedComs of
-        [] -> (stack $ expDoc ++ [ppr v], 1000)
-        _ -> 
-          if checkComment (map snd numberedComs) sl then do
-            let tmpdoc = expDoc ++ [align . text . unpackCommentString $ head $ map snd numberedComs]
-            formatExp exp (tail numberedComs) tmpdoc
-          else (stack $ expDoc ++ [ppr v], fst (head numberedComs))
-
--- | Run @futhark fmt@.
-main :: String -> [String] -> IO ()
-main = mainWithOptions () [] "program" $ \args () ->
-  case args of
-    [file] -> Just $ do
-      s <- TIO.readFile file
-      case parseWithComments file s of
-        (Left e, _) -> do
-          putStr $ "Syntax error: " ++ locStr (syntaxErrorLoc e) ++ "\n" ++ syntaxErrorMsg e
-          putStrLn "File has not been changed."
-        (Right prog, comments) -> do
-          case prog of
-            Prog doc decs -> do
-              --print $ head decs
-              writeFile ("fmt." ++ file) $ PP.pretty 80 $ prettyDocComment doc <> formatSource decs comments [] -- write fmt to file
-              --putStrLn $ PP.pretty 80 $ prettyDocComment doc <> prettySource decs comments --write fmt to stdout
-    _ -> Nothing
-
-
 prettyDocComment :: Maybe DocComment -> Doc
 prettyDocComment m_dc =
   case m_dc of
@@ -147,54 +61,67 @@ docstring ('\n' : s) = line <> text "-- " <> docstring s
 docstring s = case span (/= '\n') s of
   (xs, ys) -> text xs <> docstring ys
 
+-- TODO: how do we avoid next dec to re-print comments from last dec's area?
+formatSource :: [UncheckedDec] -> [L Token] -> Doc
+formatSource decs coms =
+  stack $ map formatDec decs
+  where
+    formatDec dec = 
+      case dec of
+        ValDec vbb -> formatValBind vbb coms mempty
+        _ -> text "NOT IMPLEMENTED YET"
 
--- inserts comment on line above and pprs exp
-checkComment' :: Pretty a => SrcLoc -> [L Token] -> a -> Doc
-checkComment' loc comments v  =
-  if head comments `isLocatedBefore` loc then
-    (string . unpackCommentString $ head comments) </> ppr v
-  else
-    ppr v 
+    formatValBind (ValBind entry name retdecl rettype tparams args body docComment attrs sloc) coms' doc = do
+      let vbb = ValBind entry name retdecl rettype tparams args body docComment attrs sloc
+      if checkComment coms' sloc then do
+        let tmp = doc <> (align . text . unpackCommentString $ head coms')
+        formatValBind vbb (tail coms') tmp
+      else do
+        doc <> constructDocWithoutBody vbb </> indent 2 (constructExpDoc body coms')
 
+    constructDocWithoutBody (ValBind entry name retdecl rettype tparams args _ docComment attrs _) = 
+      mconcat (map ((<> line) . ppr) attrs)
+        <> prettyDocComment docComment
+        <> text fun
+        <+> pprName name
+        <+> align (sep (map ppr tparams ++ map ppr args))
+        <> retdecl'
+        <> text " = "
+      where
+        fun
+          | isJust entry = "entry"
+          | otherwise = "def"
+        retdecl' = case (ppr <$> unAnnot rettype) `mplus` (ppr <$> retdecl) of
+          Just rettype' -> colon <+> align rettype'
+          Nothing -> mempty
+    
+    constructExpDoc exp coms' = do
+      case exp of
+        IntLit v _ sloc -> 
+          if checkComment coms' sloc then do
+            (align . text . unpackCommentString $ head coms') </> constructExpDoc exp (tail coms')
+          else ppr v
+        _ -> text "NOT IMPLEMENTED"
 
-prettyExp' :: ExpBase NoInfo Name -> [L Token] -> Doc
-prettyExp' e comments = case e of
-  Literal v loc -> case comments of
-    [] -> ppr v
-    _ -> checkComment' loc comments v
-  IntLit v _ loc -> case comments of
-    [] -> ppr v
-    _ -> if head comments `isLocatedBefore` loc then 
-      (string . unpackCommentString $ head comments) <> line <> prettyExp' e (tail comments)
-      else ppr v
-  FloatLit v _ loc -> checkComment' loc comments v <> ppr v
-  StringLit s loc -> checkComment' loc comments s <> text (show $ map (chr . fromIntegral) s)
-  Hole ni loc -> mempty -- TODO: support for hole
-  Var name t loc -> checkComment' loc comments name <> ppr name <> inst
-    where
-      inst = case unAnnot t of
-        Just t'
-          | isEnvVarAtLeast "FUTHARK_COMPILER_DEBUGGING" 2 ->
-              text "@" <> parens (align $ ppr t')
-        _ -> mempty
-  Parens e loc -> ppr e
-  QualParens (v, _) e loc -> ppr e
-  TupLit es loc -> ppr e
-  RecordLit fs loc -> ppr e
-  ArrayLit es info loc -> ppr e
-  Attr attr e loc -> ppr e
-  Project k e _ loc -> ppr e
-  Negate e loc -> ppr e
-  Not e loc -> ppr e
-  Assert e1 e2 _ loc -> ppr e
-  Constr n cs _ loc -> ppr e
-  Update src idxs ve loc -> ppr e
-  RecordUpdate src fs ve _ loc -> ppr e
-  Lambda params body rettype _ loc -> ppr e
-  OpSection binop _ loc -> ppr e
-  OpSectionLeft binop _ x _ _ loc -> ppr e
-  OpSectionRight binop _ x _ _ loc -> ppr e
-  ProjectSection fields _ loc -> ppr e
-  IndexSection idxs _ loc -> ppr e
-  Ascript e t loc -> ppr e
-  AppExp e loc -> ppr e
+    restComments doc coms' = 
+      case coms' of
+        [] -> doc
+        _ -> restComments (doc </> (text . unpackCommentString $ head coms')) (tail coms')
+
+-- | Run @futhark fmt@.
+main :: String -> [String] -> IO ()
+main = mainWithOptions () [] "program" $ \args () ->
+  case args of
+    [file] -> Just $ do
+      s <- TIO.readFile file
+      case parseWithComments file s of
+        (Left e, _) -> do
+          putStr $ "Syntax error: " ++ locStr (syntaxErrorLoc e) ++ "\n" ++ syntaxErrorMsg e
+          putStrLn "File has not been changed."
+        (Right prog, comments) -> do
+          case prog of
+            Prog doc decs -> do
+              --print $ head decs
+              writeFile ("fmt." ++ file) $ PP.pretty 80 $ prettyDocComment doc <> formatSource decs comments -- write fmt to file
+              --putStrLn $ PP.pretty 80 $ prettyDocComment doc <> prettySource decs comments --write fmt to stdout
+    _ -> Nothing
