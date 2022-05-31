1cec83a59ba58ac8e768f77ac41fa357d367d251
Sunese <suneengtorp@live.dk>
2022-05-31 19:23:12 +0200

    working on comments inside of letpat, bit buggy and janky, but getting there

--- src/Futhark/CLI/Fmt.hs
+++ src/Futhark/CLI/Fmt.hs
@@ -54,6 +54,29 @@ docstring ('\n' : s) = line <> text "-- " <> docstring s
 docstring s = case span (/= '\n') s of
   (xs, ys) -> text xs <> docstring ys
 
+letBody :: (Eq vn, IsName vn, Annot f) => ExpBase f vn -> Doc -> Doc
+letBody body@(AppExp LetPat {} _) _ = ppr body
+letBody body@(AppExp LetFun {} _) _ = ppr body
+letBody body comsDoc = text "in" <+> align comsDoc </> align (ppr body)
+
+genComsDoc :: [(Int, L Token)] -> SrcLoc -> Doc -> (Doc, Int)
+genComsDoc comments sloc doc = 
+  case comments of 
+    [] -> (doc, 10000)
+    _ -> 
+      if checkComment comments sloc
+        then genComsDoc -- insert comment (and newline) and recurse
+              (tail comments) 
+              sloc 
+              (doc </> text (unpackCommentString (head (unzipComs comments))))
+        else (doc, fst $ head comments)
+  
+hasArrayLit :: ExpBase ty vn -> Bool
+hasArrayLit ArrayLit {} = True
+hasArrayLit (TupLit es2 _) = any hasArrayLit es2
+hasArrayLit _ = False
+
+
 formatSource :: [UncheckedDec] -> [L Token] -> Doc -> Doc
 formatSource decs coms doc =
   case decs of
@@ -104,7 +127,7 @@ formatSource decs coms doc =
           Nothing -> mempty
 
     formatTypeBind (TypeBind name l params te _ docComment _) coms' = do
-      let (expDoc, consumed) = constructExpDoc te coms' mempty
+      let (expDoc, consumed) = constructTypeExpDoc te coms' mempty
       (prettyDocComment docComment <>
         text "type" <> ppr l <+> pprName name
         <+> spread (map ppr params)
@@ -112,11 +135,53 @@ formatSource decs coms doc =
         </> indent 2 expDoc, consumed)
 
     constructExpDoc exp coms' expDoc =
+      -- maybe just checkComment srclocOf exp <> ppr exp?
       case coms' of 
         [] -> (expDoc </> ppr exp, 10000)
         _ ->  if checkComment coms' (srclocOf exp) then do
                 let tmpDoc = expDoc </> (align . text . unpackCommentString $ head $ unzipComs coms')
                 constructExpDoc exp (tail coms') tmpDoc
+              else --(expDoc </> ppr exp, fst $ head coms')
+                case exp of 
+                  -- no support for TupLit, ArrayLit yet
+                  Parens e _ -> constructExpDoc e coms' expDoc
+                  AppExp e _ -> do
+                    case e of
+                      DoLoop sizeparams pat initexp form loopbody _ -> do
+                        let (doc', consumed) = constructExpDoc loopbody coms' expDoc
+                        (text "loop"
+                          <+> align
+                            ( spread (map (brackets . pprName) sizeparams)
+                                <+/> ppr pat <+> equals
+                                <+/> ppr initexp
+                                <+/> ppr form <+> text "do"
+                            )
+                          </> indent 2 doc', consumed)
+                      LetPat sizes pat e body sloc -> do
+                        let (comsDoc, consumed) = genComsDoc coms' (srclocOf body) mempty
+                        (parensIf (0 /= -1) $
+                          align $
+                            text "let" <+> spread (map ppr sizes) <+> align (ppr pat)
+                              <+> ( if linebreak
+                                      then equals </> indent 2 (ppr e)
+                                      else equals <+> align (ppr e)
+                                  )
+                              </> comsDoc </> letBody body comsDoc, consumed)
+                        where
+                          linebreak = case e of
+                            AppExp {} -> True
+                            Attr {} -> True
+                            ArrayLit {} -> False
+                            _ -> hasArrayLit e
+                      _ -> (expDoc </> ppr exp, fst $ head coms')
+                  _ -> (expDoc </> ppr exp, fst $ head coms')
+
+    constructTypeExpDoc exp coms' expDoc =
+      case coms' of 
+        [] -> (expDoc </> ppr exp, 10000)
+        _ ->  if checkComment coms' (srclocOf exp) then do
+                let tmpDoc = expDoc </> (align . text . unpackCommentString $ head $ unzipComs coms')
+                constructTypeExpDoc exp (tail coms') tmpDoc
               else (expDoc </> ppr exp, fst $ head coms')
 
 -- | Run @futhark fmt@.
@@ -131,13 +196,14 @@ main = mainWithOptions () [] "program" $ \args () ->
           putStrLn "File has not been changed."
         (Right prog, comments) -> do
           case prog of
-            Prog doc decs -> do
+            Prog doc decs ->
               --print $ head decs
-              writeFile ("fmt." ++ file) 
-                $ trim
-                $ PP.pretty 80 
-                $ prettyDocComment doc 
-                  <> formatSource decs comments mempty -- write fmt to file
+              --writeFile file $ show $ head decs 
+              writeFile ("fmt." ++ file)
+              $ trim
+              $ PP.pretty 80 
+              $ prettyDocComment doc 
+              <> formatSource decs comments mempty -- write fmt to file
               --print $ zip [0..] comments
               --print $ (locOf $ srclocOf $ tail decs) > (unpackTokLoc $ comments!!2)
               --putStrLn $ PP.pretty 80 $ prettyDocComment doc <> prettySource decs comments --write fmt to stdout
